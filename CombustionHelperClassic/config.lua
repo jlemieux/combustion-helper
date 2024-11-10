3acc0a83f71865e3920c3c8443790062643c8a26
James Lemieux <jimmygtr11@gmail.com>
2024-11-10 01:22:45 -0500

    splits into 2 files

---
+++ CombustionHelperClassic/config.lua
@@ -0,0 +1,66 @@
+local addonName, ns = ...;
+
+ns.AceConfig = LibStub("AceConfig-3.0")
+ns.AceConfigDialog = LibStub("AceConfigDialog-3.0")
+ns.AceConsole = LibStub("AceConsole-3.0")
+
+local dbDefaults = {
+	profile = {
+		message = "Welcome Home!",
+		showOnScreen = true,
+	},
+}
+
+local interfaceOptions = {
+	name = addonName,
+	type = "group",
+	args = {
+		message = {
+			type = "input",
+			name = "Message",
+			desc = "The message to be displayed when you get home.",
+			usage = "<Your message>",
+			get = function(info)
+        return ns.db.profile.message
+      end,
+			set = function(info, value)
+        ns.db.profile.message = value
+      end,
+		},
+		showOnScreen = {
+			type = "toggle",
+			name = "Show on Screen",
+			desc = "Toggles the display of the message on the screen.",
+			get = function(info)
+        return ns.db.profile.showOnScreen
+      end,
+			set = function(info, value)
+        ns.db.profile.showOnScreen = value
+      end,
+		},
+	},
+}
+
+function ns.initConfig()
+  -- set up db
+  ns.db = LibStub("AceDB-3.0"):New(addonName.."DB", dbDefaults, true)
+  -- set up profiles using the db
+	local profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(ns.db)
+
+  -- register AceConfig options
+	ns.AceConfig:RegisterOptionsTable(addonName.."_options", interfaceOptions)
+  ns.AceConfig:RegisterOptionsTable(addonName.."_Profiles", profiles)
+
+  -- add top level options frame to interface
+	local interfaceOptionsFrame = ns.AceConfigDialog:AddToBlizOptions(addonName.."_options", addonName)
+	-- add child 'Profiles' under top level options
+	ns.AceConfigDialog:AddToBlizOptions(addonName.."_Profiles", "Profiles", addonName)
+
+
+  -- register chat commands
+  local function OpenInterfaceOptions()
+    Settings.OpenToCategory(interfaceOptionsFrame.name)
+  end
+  ns.AceConsole:RegisterChatCommand("chc", OpenInterfaceOptions)
+	ns.AceConsole:RegisterChatCommand(addonName, OpenInterfaceOptions)
+end
